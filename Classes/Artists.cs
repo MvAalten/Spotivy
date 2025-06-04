b718a7de8c5e808f7ef5efacb74e643b7a0251e5
MvAalten <157478216+MvAalten@users.noreply.github.com>
2025-06-04 09:59:36 +0200

    in artist there can be artists with albums and songs. you can add songs to a playlist, showplayables returns all of the songs from the playlist. in Song there is a function that makes a song with a title artist time songgenre for every artist. also returns the time of the song and returns all the song data

--- Classes/Artists.cs
+++ Classes/Artists.cs
@@ -4,14 +4,17 @@ using System.Collections.Generic;
 public class Artist
 {
     public string Naam { get; private set; }
-
     public List<Album> Albums { get; private set; }
-
     public List<Song> Songs { get; private set; }
 
+    /**
+     * Creates a new Artist with the specified name and a list of albums.
+     */
     public Artist(string naam, List<Album> albums)
     {
-        throw new NotImplementedException();
+        Naam = naam;
+        Albums = albums;
+        Songs = new List<Song>();
     }
 
     public void AddSong(Song song)
