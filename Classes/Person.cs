6ce06af61d5a57b3c51a274a6131562d669b0a62
MvAalten <157478216+MvAalten@users.noreply.github.com>
2025-06-23 11:03:02 +0200

    feat: implement user profile viewing functionality
    Add profile display with name, friends, and playlists
    Implement ShowAllUsers() and SelectUser() methods
    omplete Person class constructor and display methods

--- Classes/Person.cs
+++ Classes/Person.cs
@@ -5,34 +5,70 @@ using System.Collections.Generic;
 
 public class Person
 {
-    public string Naam { get; private set; }
+    public string Name { get; private set; }
 
     public List<Person> Friends { get; private set; }
 
     public List<Playlist> Playlists { get; private set; }
 
-    public Person(string naam)
+    public Person(string name)
     {
-        throw new NotImplementedException();
-    }
+        Name = name;
+        Friends = new List<Person>();
+        Playlists = new List<Playlist>();    }
 
+    /**
+     * If you have no friends return
+     *
+     * If you have friends return friends number with the name
+     */
     public string ShowFriends()
     {
-        throw new NotImplementedException();
+        if (Friends.Count == 0)
+        {
+            return "No friends added yet.";
+        }
+
+        string friendsList = "Friends:\n";
+        for (int i = 0; i < Friends.Count; i++)
+        {
+            friendsList += $"{i + 1}. {Friends[i].Name}\n";
+        }
+        return friendsList;
+        
     }
 
+    /**
+     * If you have no playlists return
+     *
+     * If you have playlists return playlist number with the title
+     */
     public string ShowPlaylists()
     {
-        throw new NotImplementedException();
+        if (Playlists.Count == 0)
+        {
+            return "No playlists created yet.";
+        }
+
+        string playlistsList = "Playlists:\n";
+        for (int i = 0; i < Playlists.Count; i++)
+        {
+            playlistsList += $"{i + 1}. {Playlists[i].Title}\n";
+        }
+        return playlistsList;
+        
     }
 
     public List<Playlist> SelectPlaylists()
     {
-        throw new NotImplementedException();
+        return Playlists;
     }
 
+    /**
+     * returns profile name, Friends count and playlist count.
+     */
     public override string ToString()
     {
-        throw new NotImplementedException();
+        return $"Profile: {Name} | Friends: {Friends.Count} | Playlists: {Playlists.Count}";
     }
 }
